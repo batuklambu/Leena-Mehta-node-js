ac6a20d7cfa93435401f0a5a60b680a1d3971ab9
Leenamehta <>
2022-11-29 23:37:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+
+for(let i =10;i<=90;i=i+20){
+  console.log(i)
+}
+console.log("..................................")
+for(let i =20;i<=100;i=i+20){
+  console.log(i)
+}
+console.log("..................................")
+
+let grade = 90;
+if (grade >= 80) {
+  console.log("You did a good job");
+} else if (grade >= 70) {
+  console.log("Keep tryingb");
+} else if (grade >= 60) {
+  console.log("ehhhh");
+} else if (grade >= 55) {
+  console.log("Not to good");
+} else {
+  console.log("is Bad Grade But You'll Get Em Next Time");
+}
+ grade = 75;
+if (grade >= 80) {
+  console.log("You did a good job");
+} else if (grade >= 70) {
+  console.log("Keep trying");
+} else if (grade >= 60) {
+  console.log("ehhhh");
+} else if (grade >= 55) {
+  console.log("Not to good");
+} else {
+  console.log("is Bad Grade But You'll Get Em Next Time");
+}
+ grade = 66;
+if (grade >= 80) {
+  console.log("You did a good job");
+} else if (grade >= 70) {
+  console.log("Keep trying");
+} else if (grade >= 60) {
+  console.log("ehhhh");
+} else if (grade >= 55) {
+  console.log("Not to good");
+} else {
+  console.log("is Bad Grade But You'll Get Em Next Time");
+}
+grade = 56;
+if (grade >= 80) {
+  console.log("You did a good job");
+} else if (grade >= 70) {
+  console.log("Keep trying");
+} else if (grade >= 60) {
+  console.log("ehhhh");
+} else if (grade >= 55) {
+  console.log("Not to good");
+} else {
+  console.log("is Bad Grade But You'll Get Em Next Time");
+}
+grade = 48;
+if (grade >= 80) {
+  console.log("You did a good job");
+} else if (grade >= 70) {
+  console.log("Keep trying");
+} else if (grade >= 60) {
+  console.log("ehhhh");
+} else if (grade >= 55) {
+  console.log("Not to good");
+} else {
+  console.log("is Bad Grade But You'll Get Em Next Time");
+}
